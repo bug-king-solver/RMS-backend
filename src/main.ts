5bd752a24ca64a70011cc5decf5dab99bbc8826e
jannydream <fation.qama.god@gmail.com>
2023-08-21 02:22:30 +0200

    feat: remove swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
-import { setupSwagger } from './common/configs/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,7 +9,7 @@ async function bootstrap() {
   const siteUrl = process.env.SITE_URL;
 
   app.enableCors();
-  setupSwagger(app);
+  
   await app.listen(port, () => {
     Logger.log(`Server is running at ${siteUrl}graphql`)
   });
