610fa5d74cafce54a315d9db4c3e54036381cb58
jannydream <fation.qama.god@gmail.com>
2023-08-29 02:54:53 +0200

    fix: remove swagger

--- src/books/dto/create-book.input.ts
+++ src/books/dto/create-book.input.ts
@@ -1,5 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { ApiProperty } from '@nestjs/swagger';
+
 import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
@@ -7,22 +7,18 @@ export class CreateBookInput {
   @Field(() => String)
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
   title: string;
 
   @Field(() => String)
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
   description?: string;
 
   @Field(() => String)
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
   body: string;
 
   @Field(() => Boolean, { defaultValue: false })
-  @ApiProperty()
   published: boolean;
 }
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,22 +1,17 @@
 import { ObjectType, Field } from '@nestjs/graphql';
-import { ApiProperty } from '@nestjs/swagger';
 import { BaseModel } from 'src/common/abstract-model/base.model';
 
 @ObjectType('book')
 export class Book extends BaseModel {
   @Field({ nullable: false })
-  @ApiProperty()
   title: string;
 
   @Field({ nullable: false })
-  @ApiProperty()
   description?: string;
 
   @Field({ nullable: false })
-  @ApiProperty()
   body: string;
 
   @Field({ nullable: false })
-  @ApiProperty()
   published: boolean;
 }
